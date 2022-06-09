f615bc458ac00ac6eef969c695daef8e3aacfeaf
boubouleuh <adonis@3000fr.com>
2022-06-09 14:52:34 +0200

    added banner + fixed background + some improvement

--- website/script.js
+++ website/script.js
@@ -1,3 +1,12 @@
-function test() {
-    
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const h1 = document.getElementsByTagName('h1')[0]
+    h1.style.transform = "translateX(1.2003vw)"
+}, false);
+
+// window.addEventListener("scroll", (event) => {
+//     let scrollY = this.scrollY;
+//     console.log(scrollY);
+//     if (scrollY >= "1000"){
+//         document.getElementById('imgstart').style.visibility = "visible"
+//     }
+// });
